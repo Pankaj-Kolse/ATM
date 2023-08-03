998e050cc14111e4223ebadd08e34da6785c3a99
Pankaj <kolsepankaj6@gmail.com>
2023-08-03 12:46:46 +0530

    ATM Commit

---
+++ src/com/capg/Learn/Atm.java
@@ -0,0 +1,11 @@
+package com.capg.Learn;
+
+import java.io.IOException;
+
+public class Atm extends Optional {
+    public static void main(String[] args) throws IOException {
+        Optional optional = new Optional();
+
+        optional.getLogin();
+    }
+}
